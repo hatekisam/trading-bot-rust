2bd4b8bcc86f2e2af7075dddb420f4d87792ba88
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-29 08:15:03 +0200

    started the binance api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use trading_bot::TradingBot;
+
+#[tokio::main]
+async fn main() {
+    let api_key = "your_api_key";
+    let api_secret = "your_api_secret";
+
+    let trading_bot = TradingBot::new(api_key, api_secret);
+
+    // Example: X amount
+    let amount_to_trade = 50.0;
+
+    trading_bot.execute_trade(amount_to_trade).await;
 }
